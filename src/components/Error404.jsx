bd7c8fad8e3f71dc92f63f33bd998b136c2a4a99
julian-lr <j.laurisc@gmail.com>
2023-12-09 05:45:22 -0300

    modificación del cartel de error 404

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,7 +1,7 @@
 export const Error404 = () => {
     return (
       <div className="error">
-          <h2>Creo que pusiste cualquier cosa en el link pa...</h2>
+          <h2>Este producto no existe.</h2>
       </div>
     ) 
   }
